81baedb566d09236aab15bb4023f082689ca8d2e
Евгений Константинов <kanstantsinav@gmail.com>
2021-10-27 22:39:03 +0300

    Добавьте файлы проекта.

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _58.Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string input = "Hello Worldddd   ";
+
+            Console.WriteLine(input);
+            Console.WriteLine(Convert.ToInt32(LengthOfLastWord(input)));
+            Console.Read();
+
+
+            int LengthOfLastWord(string s)
+            {
+                int temp = 0;
+                for(int i = s.Length-1; i > -1; i--)
+                {
+                    if (s[i] == ' ' && temp == 0)  continue;
+                    else if (s[i] == ' ' && temp > 0) return temp;
+                    temp += 1;
+
+                }
+                return temp;
+            }
+
+
+
+        }
+    }
+}
---
+++ 58. Length of Last Word/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("58. Length of Last Word")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("58. Length of Last Word")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1b456b72-6dfd-4acf-a0a1-71a4914550a8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
